85bee11cb8ff4d53bb57737359c9c249b660343f
Meaghan Hoitt <mlh1964@g.rit.edu>
2018-10-06 15:23:42 -0400

    updated getsinginroute

--- src/main/java/com/webcheckers/ui/GetSigninRoute.java
+++ src/main/java/com/webcheckers/ui/GetSigninRoute.java
@@ -4,17 +4,33 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.logging.Logger;
-import spark.ModelAndView;
-import spark.Request;
-import spark.Response;
-import spark.Route;
-import spark.TemplateEngine;
 
+import spark.*;
+import spark.Route;
+
+/**
+ * The {@code GET /signin} route handler.
+ *
+ * @author <a href='mailto:@rit.edu'>Simon Kirkwood</a>
+ * @author <a href='mailto:mlh1964@.rit.edu'>Meaghan Hoitt</a>
+ */
 public class GetSigninRoute implements Route {
+
+  // Values used in the view-model map for rendering the home view.
+  static final String TITLE_ATTR = "title";
+  static final String TITLE = "Sign-in";
+  static final String VIEW_NAME = "signin.ftl";
+
   private final TemplateEngine templateEngine;
   private static final Logger LOG = Logger.getLogger(GetSigninRoute.class.getName());
 
-  public GetSigninRoute(TemplateEngine templateEngine){
+  /**
+   * The constructor for the {@code GET /signin} route handler.
+   *
+   * @param templateEngine
+   *    The {@link TemplateEngine} used for rendering page HTML.
+   */
+  public GetSigninRoute(final TemplateEngine templateEngine){
     // validation
     Objects.requireNonNull(templateEngine, "templateEngine must not be null");
     //
@@ -23,12 +39,25 @@ public class GetSigninRoute implements Route {
     LOG.config("GetSigninRoute is initialized.");
     }
 
+  /**
+   * Render the WebCheckers Sign-in page.
+   *
+   * @param request
+   *   the HTTP request
+   * @param response
+   *   the HTTP response
+   *
+   * @return
+   *   the rendered HTML for the Sign-in page
+   */
+  @Override
   public Object handle(Request request, Response response){
     LOG.finer("GetHomeRoute is invoked.");
-    //
-    Map<String, Object> vm = new HashMap<>();
-    vm.put("title", "Sign in!");
 
-    return templateEngine.render(new ModelAndView(vm , "signin.ftl"));
+    //start building the View-Model
+    final Map<String, Object> vm = new HashMap<>();
+    vm.put(TITLE_ATTR, TITLE);
+
+    return templateEngine.render(new ModelAndView(vm ,VIEW_NAME));
   }
 }
