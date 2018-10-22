abc6245b03842caee77f1daf25c84257e63714bb
Meaghan Hoitt <mlh1964@g.rit.edu>
2018-10-21 23:34:16 -0400

    Changed everyones email to not be mine. . .

--- src/main/java/com/webcheckers/application/GameCenter.java
+++ src/main/java/com/webcheckers/application/GameCenter.java
@@ -13,9 +13,9 @@ import java.util.logging.Logger;
  *  </p>
  *
  *  @author <a href='mailto:mlh1964@rit.edu'>Meaghan Hoitt</a>
- *  @author <a href='mailto:mlh1964@rit.edu'>Simon Kirwkwood</a>
- *  @author <a href='mailto:mlh1964@rit.edu'>Matthew Milone</a>
- *  @author <a href='mailto:mlh1964@rit.edu'>Andrew Festa</a>
+ *  @author <a href='mailto:sjk7867@rit.edu'>Simon Kirwkwood</a>
+ *  @author <a href='mailto:mvm7902@rit.edu'>Matthew Milone</a>
+ *  @author <a href='mailto:axf5592@rit.edu'>Andrew Festa</a>
  *
  */
 public class GameCenter {
--- src/main/java/com/webcheckers/ui/boardView/BoardView.java
+++ src/main/java/com/webcheckers/ui/boardView/BoardView.java
@@ -12,9 +12,9 @@ import java.util.Iterator;
  *  </p>
  *
  *  @author <a href='mailto:mlh1964@rit.edu'>Meaghan Hoitt</a>
- *  @author <a href='mailto:mlh1964@rit.edu'>Simon Kirwkwood</a>
- *  @author <a href='mailto:mlh1964@rit.edu'>Matthew Milone</a>
- *  @author <a href='mailto:mlh1964@rit.edu'>Andrew Festa</a>
+ *  @author <a href='mailto:sjk7867@rit.edu'>Simon Kirwkwood</a>
+ *  @author <a href='mailto:mvm7902@rit.edu'>Matthew Milone</a>
+ *  @author <a href='mailto:axf5592@rit.edu'>Andrew Festa</a>
  */
 public class BoardView implements Iterable {
   //
