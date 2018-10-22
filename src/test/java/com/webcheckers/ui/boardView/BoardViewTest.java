adbcdb2735c9b2e68939de86f9a86318e9f9f11a
Adrang <axf5592@rit.edu>
2018-10-21 23:47:11 -0400

    Created unit test classes for the BoardView ui classes Initial implementation of SignOut Route

---
+++ src/test/java/com/webcheckers/ui/boardView/BoardViewTest.java
@@ -0,0 +1,22 @@
+package com.webcheckers.ui.boardView;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BoardViewTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void iterator() {
+  }
+}
