3062434f20f99a291f45df867e2562ef7de825b6
sjk7867 <sjk7867@rit.edu>
2018-11-06 20:43:41 -0500

    Fixed a conflict

--- src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
+++ src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
@@ -79,28 +79,13 @@ public class PostValidateMoveRoute implements Route {
   public Object handle(Request request, Response response) {
     final Session session = request.session();
     Player player = session.attribute("player");
-
-    if(player == null || playerLobby.getPlayer(player.getName()) == null) {
-      return new Message("Invalid move", MESSAGE_TYPE.error);
-    }
-
     Game game = gameCenter.getGames(player)[0];
     LOG.finer("PostValidateMoveRoute is invoked: " + player.getName());
 
-<<<<<<< HEAD
     Move requestMove = gson.fromJson(request.body(), Move.class);
     if(game.validateMove(requestMove)){
-=======
-    //
-    Map<String, LinkedTreeMap> map = gson.fromJson(request.body(), Map.class);
-    LinkedTreeMap startPos = map.get("start");
-    LinkedTreeMap endPos = map.get("end");
-    if(game.validateMove(0,0,0,0)){
-      game.setLastTurn(true);
->>>>>>> TurnTracking
       return new Message("Valid move", MESSAGE_TYPE.info);
     } else {
-      game.setLastTurn(false);
       return new Message("Invalid move", MESSAGE_TYPE.error);
     }
   }
