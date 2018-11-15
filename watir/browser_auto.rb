4f5d3dd94b43c10310ec2c0efb54c84e76478c7a
Adrang <axf5592@rit.edu>
2018-11-14 19:20:07 -0500

    Built out more of the web automation script

--- watir/browser_auto.rb
+++ watir/browser_auto.rb
@@ -1,10 +1,14 @@
 require 'watir'
 
-browser = Watir::Browser.new
-browser.goto 'google.com'
-browser.text_field(title: 'Search').set 'Hello World!'
-browser.button(type: 'submit').click
+browser0 = Watir::Browser.new :chrome
+browser0.goto '127.0.0.1:4567'
 
-puts browser.title
+browser1 = Watir::Browser.new :chrome
+browser1.goto '127.0.0.2:4567'
+
+# browser.text_field(title: 'Search').set 'Hello World!'
+# browser.button(type: 'submit').click
+
+# puts browser.title
 # => 'Hello World! - Google Search'
-browser.close
+# browser.close
