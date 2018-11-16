e1d61ae38b55ed395bf0810be17a5bac93e89b09
Matthew Milone <mvm7902@rit.edu>
2018-11-16 13:38:45 -0500

    Added promotePiece function under Board.

--- src/main/java/com/webcheckers/model/Board.java
+++ src/main/java/com/webcheckers/model/Board.java
@@ -394,6 +394,21 @@ public class Board implements Cloneable {
     return remPiece;
   }
 
+  /*
+  * Changes a single piece to a king.
+  *
+  * @param   x x coordinate on the cartesian board
+  * @param   y y coordinate on the cartesian board
+  * @return    the promoted piece
+  */
+  SPACE_TYPE promotePiece(int x, int y){
+    SPACE_TYPE piece = getPieceAtLocation(x, y);
+    int bitIdx = cartesianToIndex(x, y);
+    int bitMask = 1 << bitIdx;
+    pieceTypes |= bitMask;
+    return piece;
+  }
+
   /**
    * TODO
    */
