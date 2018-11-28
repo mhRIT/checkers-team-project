2a5860efd1d4450895b395f9fd98c07716c482f2
Matthew Milone <mvm7902@rit.edu>
2018-11-27 21:15:15 -0500

    Fixed inverted piece colors.

--- src/main/java/com/webcheckers/model/Board/Board.java
+++ src/main/java/com/webcheckers/model/Board/Board.java
@@ -221,7 +221,7 @@ public class Board implements Cloneable {
   void initMid() {
     // TODO
     pieceLocations =  0b0101_0011_1111_1111_1100_1111_1111_0010;
-    pieceColors =     0b0000_0000_0000_0011_1000_1110_1111_0010;
+    pieceColors =     0b0001_0001_1110_1110_0100_0001_0000_0000;
     pieceTypes =      0b0000_0000_0000_0000_0000_0000_0000_0000;
   }
 
@@ -232,7 +232,7 @@ public class Board implements Cloneable {
    */
   void initEnd() {
     pieceLocations =  0b0110_0010_1000_0100_1000_0101_1000_0110;
-    pieceColors =     0b0110_0010_0000_0100_1000_0000_1000_0010;
+    pieceColors =     0b0000_0000_1000_0000_0000_0101_0000_0100;
     pieceTypes =      0b0110_0010_0000_0000_0000_0000_0000_0100;
   }
 
